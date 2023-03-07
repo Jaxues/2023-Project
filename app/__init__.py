d399afe661e7908e77fded4789fa7f471b627d25
Jack <jholmesoliver@gmail.com>
2023-03-07 19:27:47 +1300

    Added gitignore to hide .env file >:)

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,4 @@ app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Hadit.db'
 app.config['SECRET_KEY']=environ['secret_key']
 db=SQLAlchemy(app)
-
 from app import forms, models, routes
